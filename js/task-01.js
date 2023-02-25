00744a4f2cbf987fc4083de403171c9a0ee495e4
cmdspeed <cmdspeed@gmail.com>
2023-02-25 11:19:10 +0100

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 console.log('Number of categories: ' + document.querySelectorAll('.item').length);
 document.querySelectorAll('.item').forEach(item => {
-  console.log('category: ' + item.firstElementChild.textContent);
+  console.log('\n' + 'category: ' + item.firstElementChild.textContent);
   console.log('Elements: ' + item.lastElementChild.children.length);
 });
