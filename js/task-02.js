d910fbea578be8b7e2dcbd75257bd084eedac2ee
cmdspeed <cmdspeed@gmail.com>
2023-02-25 11:48:39 +0100

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.getElementById('ingredients');
+
+for (let text of ingredients) {
+  let list = document.createElement('li');
+  list.textContent = text;
+  list.classList = 'item';
+  ingredientsList.append(list);
+}
