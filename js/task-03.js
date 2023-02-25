7ea2c53ff161ecdeda48020dcbc92c27d887f8a3
cmdspeed <cmdspeed@gmail.com>
2023-02-25 13:04:49 +0100

    task 03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const list = document.querySelector('.gallery');
+
+list.insertAdjacentHTML(
+  'beforeend',
+  images.map(image => `<li><img src="${image.url}" alt="${image.alt}"/></li>`).join('')
+);
