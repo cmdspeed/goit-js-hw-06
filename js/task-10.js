367cd8c3a91e56122f9f93d986a767b4519f2ef4
cmdspeed <cmdspeed@gmail.com>
2023-02-26 14:55:29 +0100

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+let numberDiv = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  const allDiv = [];
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.height = `${30 + 10 * i}px`;
+    div.style.width = `${30 + 10 * i}px`;
+    div.style.background = getRandomHexColor();
+    allDiv.push(div);
+  }
+
+  return allDiv;
+}
+
+btnCreate.addEventListener('click', () => {
+  let allBox = createBoxes(numberDiv.value);
+
+  boxes.append(...allBox);
+});
+
+btnDestroy.addEventListener('click', () => {
+  boxes.innerHTML = '';
+});
